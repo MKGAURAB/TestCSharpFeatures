e2988dc8d3956a7852fc2c529bb1737e1df8ee97
khairullah <Khairullah.gau@2c2p.com>
2020-04-18 14:18:52 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace OptionType
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    class Option<T> : IEnumerable<T>
+    {
+        private IEnumerable<T> Content { get; }
+
+        private Option(IEnumerable<T> content)
+        {
+            Content = content;
+        }
+
+        public static Option<T> Some(T value) => new Option<T>(new [] {value});
+
+        public static Option<T> None() => new Option<T>(new T[0]);
+        public IEnumerator<T> GetEnumerator() => this.Content.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+
+    static class EnumerableExtensions
+    {
+        public static void PerformAction<T>(this IEnumerable<T> sequence, Action<T> action)
+        {
+            foreach (T item in sequence)
+            {
+                action(item);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Option<string> test;
+
+            test = Option<string>.None();
+            test.PerformAction(item => Console.WriteLine($"I am {item}"));
+
+            test = Option<string>.Some("Gaurab");
+            test.PerformAction(item => Console.WriteLine($"I am {item}"));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
